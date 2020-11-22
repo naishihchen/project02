8224b94ad33a28dcd451f3f896e2b81b071170e4
Nai Shih Chen <che17030@byui.edu>
2020-11-21 20:52:42 -0700

    Added Picture Search

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+var parser = require('body-parser');
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(parser.urlencoded({ extended: false }))
   .use(express.static(path.join(__dirname, 'public')))
+  .use(parser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
