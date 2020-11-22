d9073aced61adf79d7a338dd55515a9ee21d9009
Nai Shih Chen <che17030@byui.edu>
2020-11-21 18:17:08 -0700

    Added Picture Search

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .post('/dateSubmit', (req, res) => {
+  	var data = "";
+  	const spaceURL = 'https://api.nasa.gov/planetary/apod?api_key=Aj3aZZ1ZX9H5YfwheMgVWGyYVwghvILvpOU9MGvt&date=' + req.body.searchdate;
+
+  	fetch(spaceURL)
+  	.then(response => data = response.json())
+
+  	res.send(data);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
