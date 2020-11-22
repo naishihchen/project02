d3db847212faec0007f05eb6ac4f2e29f717cc56
Nai Shih Chen <che17030@byui.edu>
2020-11-21 20:55:06 -0700

    Added Picture Search

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 var parser = require('body-parser');
+const fetch = require('node-fetch');
 const PORT = process.env.PORT || 5000
 
 express()
@@ -17,8 +18,10 @@ express()
   	const spaceURL = 'https://api.nasa.gov/planetary/apod?api_key=Aj3aZZ1ZX9H5YfwheMgVWGyYVwghvILvpOU9MGvt&date=' + req.body.searchdate;
 
   	fetch(spaceURL)
-  	.then(response => data = response.json())
-
-  	res.send(data);
+  	.then(response => response.json())
+  	.then(data => {
+  		console.log(data)
+  		res.send(data)
+  	})
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
